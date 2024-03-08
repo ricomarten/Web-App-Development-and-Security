e129327526c66a917985412dcf212cfded9dfba3
Rico Martenstyaro <ricomarten@gmail.com>
2024-03-08 20:45:38 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+//ini perubahan rico
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
